7c210d28eb98dbce90a94cda29500449f16fc443
leomath42 <leonardomatheus042@gmail.com>
2021-08-26 22:14:36 -0300

    Exception handler and refactoring Exception classes

--- src/main/java/com/devsuperior/bds02/service/exception/DataBaseException.java
+++ src/main/java/com/devsuperior/bds02/service/exception/DataBaseException.java
@@ -1,6 +1,11 @@
-package com.devsuperior.bds02.exception;
+package com.devsuperior.bds02.service.exception;
 
 public class DataBaseException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public DataBaseException(String string) {
 		super(string);
--- src/main/java/com/devsuperior/bds02/service/exception/ResourceNotFoundException.java
+++ src/main/java/com/devsuperior/bds02/service/exception/ResourceNotFoundException.java
@@ -1,6 +1,11 @@
-package com.devsuperior.bds02.exception;
+package com.devsuperior.bds02.service.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(String string) {
 		super(string);
